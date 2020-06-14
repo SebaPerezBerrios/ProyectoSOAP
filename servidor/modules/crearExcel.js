00247243d4a0bd086999e2d5b888383f2e7621ad
Sebastian Perez Berrios <sebastian.perezb@utem.cl>
2020-06-13 23:10:37 -0400

    Implementacion algoritmo seleccion puntajes
    Se implementa el algoritmo de seleccion de mejor ponderacion posible para cada rut.
    Refactorizaciones varias.

--- servidor/modules/crearExcel.js
+++ servidor/modules/crearExcel.js
@@ -3,27 +3,27 @@ const Excel = require('exceljs');
 const { agregarPonderaciones } = require('./ponderaciones');
 const { getDatosCarreras } = require('../utils/carreras');
 
-const generarHoja = (book, { estado, nombreHoja, vacantes }) => {
-  let hoja = book.addWorksheet(nombreHoja);
-  hoja.addRow(['RUT', 'Ponderación']).commit();
-
-  estado.postulantes
-    .forEach(({ rut, ponderacion }) => {
-      hoja.addRow([rut, ponderacion]).commit();
-    });
-}
-
 const generarExcel = async (lineas, streamSalida) => {
   const opciones = {
     stream: streamSalida,
   };
   let book = new Excel.stream.xlsx.WorkbookWriter(opciones);
-  const datosCarreras = await getDatosCarreras(book);
+  const datosCarreras = await getDatosCarreras();
 
-  await agregarPonderaciones(lineas, datosCarreras);
+  agregarPonderaciones(lineas, datosCarreras);
 
-  datosCarreras.forEach((carrera) => generarHoja(book, carrera));
-  book.commit()
+  datosCarreras.forEach((carrera) => generarHojaCarrera(book, carrera));
+  book.commit();
+}
+
+const generarHojaCarrera = (book, { estado, nombreHoja }) => {
+  let hoja = book.addWorksheet(nombreHoja);
+  hoja.addRow(['RUT', 'Ponderación']).commit();
+
+  estado.seleccionados
+    .forEach(({ rut, ponderacion }) => {
+      hoja.addRow([rut, ponderacion]).commit();
+    });
 }
 
 module.exports = {
